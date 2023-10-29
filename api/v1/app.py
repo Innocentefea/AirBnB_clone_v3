6aec3d358504241204bdcd0a23a08ba74b302e7f
Innocentefea <innocentefeakpoyibo@gmail.com>
2023-10-29 12:12:58 -0700

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,8 +16,7 @@ def teardown_appcontext(exception):
     storage.close()
 
 @app.errorhandler(404)
-"""Designers are really creative when
-they have to design a “404 page” """
+"""design a “404 page”, a “Not found” """
 def not_found(error):
     response = jsonify({'error': 'Not found'})
     response.status_code = 404
