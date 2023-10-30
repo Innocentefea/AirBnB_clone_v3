19535ef7364357c1439f464277e6bd129f063d16
muirujackson <jakezweru@gmail.com>
2023-10-30 13:00:23 -0700

    places_amenities pycodestyle

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -9,7 +9,8 @@ from models.amenity import Amenity
 from api.v1.views import app_views
 
 
-@app_views.route('/places/<place_id>/amenities', methods=['GET'], strict_slashes=False)
+@app_views.route('/places/<place_id>/amenities', methods=['GET'],
+                 strict_slashes=False)
 def get_place_amenities(place_id):
     """Retrieves the list of all Amenity objects of a Place"""
     place = storage.get(Place, place_id)
@@ -19,7 +20,8 @@ def get_place_amenities(place_id):
     return jsonify(amenities)
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_place_amenity(place_id, amenity_id):
     """Deletes a Amenity object to a Place: DELETE"""
     place = storage.get(Place, place_id)
@@ -35,7 +37,8 @@ def delete_place_amenity(place_id, amenity_id):
     return jsonify({}), 200
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'], strict_slashes=False)
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['POST'], strict_slashes=False)
 def link_place_amenity(place_id, amenity_id):
     """Link a Amenity object to a Place: POST"""
     place = storage.get(Place, place_id)
