9078748f9ea773e8d7a1fe95e0f6c9bfa0393f8b
Innocentefea <innocentefeakpoyibo@gmail.com>
2023-10-30 08:51:07 -0700

    updates

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,11 @@
 create app_views
 wilcard import
 """
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
 # Import the views
 from api.v1.views.index import *
 from api.v1.views.states import *
@@ -12,6 +17,3 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
