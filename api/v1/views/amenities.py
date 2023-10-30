94793bca0d57860395487ae1409dc0bc148c14df
muirujackson <jakezweru@gmail.com>
2023-10-30 12:50:39 -0700

    amenities pycode

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -11,11 +11,12 @@ from api.v1.views import app_views
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amenities():
     """Retrieves the list of all Amenity objects: GET"""
-    amenities = [amenity.to_dict() for amenity in storage.all(Amenity).values()]
-    return jsonify(amenities)
+    ameni = [amenity.to_dict() for amenity in storage.all(Amenity).values()]
+    return jsonify(ameni)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['GET'],
+                 strict_slashes=False)
 def get_amenity(amenity_id):
     """Retrieves a Amenity object: GET"""
     amenity = storage.get(Amenity, amenity_id)
@@ -24,7 +25,8 @@ def get_amenity(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_amenity(amenity_id):
     """Deletes a Amenity object:: DELETE"""
     amenity = storage.get(Amenity, amenity_id)
@@ -48,7 +50,8 @@ def create_amenity():
     return jsonify(amenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_amenity(amenity_id):
     """Updates a Amenity object: PUT"""
     amenity = storage.get(Amenity, amenity_id)
