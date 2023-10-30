10ad2deac37d749d8475416d446477ee1d1eee4f
Innocentefea <innocentefeakpoyibo@gmail.com>
2023-10-30 07:27:58 -0700

    updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 """ api/v1/app.py"""
 from flask import Flask, jsonify
 from models import storage
+from flask_cors import CORS
 from api.v1.views import app_views
 import os
 
@@ -9,6 +10,7 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
